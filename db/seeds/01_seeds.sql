bca790245b8adfdc5383c6f4ee2b27a82c9f173d
Amy Martin <amymariemart@gmail.com>
2022-05-07 17:02:29 -0700

    Reorganize database files

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO customers (name, phone_number)
+VALUES ('Johnny Blaze', '250-896-1953'),
+('George Taylor', '250-896-1953'),
+('Bonnie Jackson', '250-896-1953');
+
+INSERT INTO batteries (name, description, image, cost, prep_time)
+VALUES ('Small Battery', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.','www.example.com', 599, 15),
+('Medium Battery', 'Elit pellentesque habitant morbi tristique senectus et netus et malesuada.Proin sagittis nisl rhoncus mattis rhoncus urna.', 'www.google.com', 1799, 30),
+('Large Battery', 'Eros donec ac odio tempor. Ipsum suspendisse ultrices gravida dictum. Et leo duis ut diam quam nulla porttitor massa.', 'www.youtube.com', 3099,  45);
+
+INSERT INTO orders (customer_id, active)
+VALUES (1, true),
+(2, true),
+(3, true);
+
+INSERT INTO battery_orders (battery_id, order_id, quantity)
+VALUES (1, 1, 3),
+(2, 1, 5),
+(3, 2, 10),
+(1, 3, 1);
