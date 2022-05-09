dac5b98acc73b33fb71bad8f44f7923f1529e304
Aidan Yee <aidan.yee@pm.me>
2022-05-09 00:52:47 +0000

    add test queries + get route for /admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,28 @@ const router = express.Router();
 
 module.exports = (pool) => {
   router.get("/", (req, res) => {
-    res.render("admin");
+    pool.query(`SELECT orders.id, customers.name, orders.created_at, orders.active, sum(batteries.cost * battery_orders.quantity) as total
+                FROM orders
+                JOIN customers ON customers.id = customer_id
+                JOIN battery_orders ON orders.id = order_id
+                JOIN batteries ON batteries.id = battery_id
+                GROUP BY orders.id, customers.name, orders.created_at, orders.active
+                ORDER BY orders.id;`)
+      .then(data => {
+        const activeOrders = {};
+        for (const order of data.rows) {
+          activeOrders[order.id] = order;
+        }
+        console.log("🚀 ~ file: admin.js ~ line 18 ~ router.get ~ activeOrders", activeOrders);
+        const templateVars = { activeOrders };
+        res.render("admin", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
-
 
   return router;
 };
