308afd0c18fc6cf9fc34a5623c210218cb1519ee
Aidan Yee <aidan.yee@pm.me>
2022-05-12 01:53:55 +0000

    add validation via jQuery for our order form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,4 +15,42 @@ $(document).ready(function() {
     menu.addClass("hidden");
     $(this).addClass("hidden");
   });
+
+  const errorMessage = $(".order-error");
+  errorMessage.hide();
+
+   $("#completeOrder").on('click', function(event) {
+    const textArea = $('input')
+    const inputText = textArea.val();
+    // event.preventDefault();
+    if (!inputText) {
+      event.preventDefault();
+      const emptyError = $('.order-error').text(`⚠️ Don't forget your Name & Phone Number!`);
+      emptyError.hide();
+      emptyError.css({
+        'color': '#FFFFFF',
+        'margin-top': '10px',
+        'display': 'inline-block',
+      }).slideDown(1500).fadeOut(4000);
+    }
+  });
+
+  // $('#completeOrder').on('click', function(event){
+  //     if($('input[name="tinyBattery"]').prop("checked") == true){
+  //         console.log("Checkbox is checked.", $('input[name="tinyBattery"]').prop("checked"));
+  //     }
+  //     else if($(this).prop("checked") == false){
+  //         console.log("Checkbox is unchecked.");
+  //     }
+  // });
+
+  // $('#completeOrder').on('click', function(event){
+  //     if(($('input[name="tinyBattery"]').prop("checked") === true & $('input[name="xsBattery"]').prop("checked") === true & $('input[name="smallBattery"]').prop("checked") === true & $('input[name="medBattery"]').prop("checked") === true & $('input[name="lgBattery"]').prop("checked")) === true & $('input[name="xlBattery"]').prop("checked") === true){
+  //         console.log("no box checked");
+  //     }
+  // });
+
+
+
 });
+
